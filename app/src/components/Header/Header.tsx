50a4214d984a7a3cc505b041c400fd6be4ac3e84
terrence-celestine <terrencebutler89@gmail.com>
2022-07-28 17:31:09 -0700

    Created secondary and primary buttons

--- app/src/components/Header/Header.tsx
+++ app/src/components/Header/Header.tsx
@@ -6,10 +6,27 @@ const Title = styled.h1`
   color: ${(props) => props.color};
 `;
 
+const PrimaryButton = styled.button`
+  background-color: #fa4169;
+  font-weight: bold;
+  font-size: 15px;
+  line-height: 17px;
+  color: #fff;
+  border-radius: 15px;
+  border: none;
+  padding: 5px 10px;
+  cursor: pointer;
+`;
+
+const SecondaryButton = styled(PrimaryButton)`
+  background-color: #4380ff;
+`;
+
 const Header = () => {
   return (
     <div>
-      <Title color="red">Hello World</Title>
+      <PrimaryButton>Primary Button</PrimaryButton>
+      <SecondaryButton>Secondary Button</SecondaryButton>
     </div>
   );
 };
