140b376d0d56fc8cbf46886dd518da2d200bf1c1
terrence-celestine <terrencebutler89@gmail.com>
2022-08-12 13:02:34 -0700

    Basic header style with proper setup

--- app/src/components/Header/Header.tsx
+++ app/src/components/Header/Header.tsx
@@ -1,34 +1,18 @@
 import styled from "styled-components";
 
-const Title = styled.h1`
-  font-size: 25px;
-  line-height: 27px;
-  color: ${(props) => props.color};
+const HeaderText = styled.h2`
+  padding: 15px;
 `;
 
-const PrimaryButton = styled.button`
-  background-color: #fa4169;
-  font-weight: bold;
-  font-size: 15px;
-  line-height: 17px;
-  color: #fff;
-  border-radius: 15px;
-  border: none;
-  padding: 5px 10px;
-  cursor: pointer;
-`;
-
-const SecondaryButton = styled(PrimaryButton)`
-  background-color: #4380ff;
+const HeaderContainer = styled.section`
+  text-align: center;
 `;
 
 const Header = () => {
   return (
-    <div>
-      <Title>Web Page Title</Title>
-      <PrimaryButton>Primary Button</PrimaryButton>
-      <SecondaryButton>Secondary Button</SecondaryButton>
-    </div>
+    <HeaderContainer>
+      <HeaderText>test</HeaderText>
+    </HeaderContainer>
   );
 };
 
